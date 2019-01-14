25da97a9073d1e31421b92ca19388df87848de21
jessix00 <jesscasillas24@gmail.com>
2019-01-13 19:26:32 -0700

    The bugs(enemy) now move accross the screen at different speeds.

--- js/app.js
+++ js/app.js
@@ -1,41 +1,31 @@
 // Enemies our player must avoid
-const Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+const Enemy = function(x, y, speed) {
+    this.x = x;
+    this.y = y + 60;
     this.sprite = 'images/enemy-bug.png';
+    this.move = 100;
+    this.boundary = this.move * 5;
+    this.resetPosition = -this.move;
+    this.speed = speed;
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+//sets boundaries and speed of enemies
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-
-    //if enemy is not passed boundry
-    //move forward
-    //increment x by speed * dt
-    //else
-    // reset possition to start
-
+    if (this.x < this.boundary) {
+        this.x += this.speed * dt;
+    } else {
+        this.x = this.resetPosition;
+    }
 };
 
-// Draw the enemy on the screen, required method for game
+// Draws the enemy on the screen
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
 //player class
 class PlayerOne {
     constructor() {
-            //properties  
             this.x = 200;
             this.y = 380;
             this.goLeftRight = 100;
@@ -84,7 +74,12 @@ class PlayerOne {
 
 }
 const player = new PlayerOne();
-
+const firstBug = new Enemy(-100, 160, 200);
+const secondBug = new Enemy(-100, 80, 300);
+const thirdBug = new Enemy(-100, 0, 150);
+const fourthBug = new Enemy(-100, 0, 200);
+const allEnemies = [];
+allEnemies.push(firstBug, secondBug, thirdBug, fourthBug);
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
